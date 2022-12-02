1fc3f589046a4767b818c69659acea845a9c3564
alidix <aliyahdixon4@gmail.com>
2022-12-02 17:40:20 -0500

    second update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+const cards = document.querySelectorAll(".memory-card");
 
+function flipCard() {
+  this.classList.toggle("flip");
+  //   console.log("I was clicked");
+  //   console.log(this);
+}
 
+cards.forEach((cards) => cards.addEventListener("click", flipCard));
