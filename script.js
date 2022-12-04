0a6a245f976ade17c0aa73b6c2054e9fb8910b7f
alidix <aliyahdixon4@gmail.com>
2022-12-03 21:30:00 -0500

    if card matches card stays up if it does not match it flips over

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ function flipCard() {
     //  Do cards match?
     // console.log(firstCard.dataset.framework);
     // console.log(secondCard.dataset.framework);
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      //  it's a match
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+      // console.log("Function was executed!");
+    } else {
+      // not a match
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+    }
   }
 }
 
