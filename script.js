0a462bcc7bd49fa44fbd17fabe4f9b7dc8cbb0f3
alidix <aliyahdixon4@gmail.com>
2022-12-03 19:43:14 -0500

    update and remove duplicate repo

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle("flip");
-  //   console.log("I was clicked");
-  //   console.log(this);
+  this.classList.add("flip");
+  //   this.classList.toggle("flip");
+  //     console.log("I was clicked");
+  //     console.log(this);
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    // console.log({ hasFlippedCard, firstCard });
+  } else {
+    hasFlippedCard;
+  }
 }
 
 cards.forEach((cards) => cards.addEventListener("click", flipCard));
